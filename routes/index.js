18b5a14eb40b4b4d07bfe9c90bc66707b3ef754b
william <william-vivier@hotmail.fr>
2021-04-04 20:18:57 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', message: 'yolo'});
+  res.render('index', { title: 'Remy le con', message: 'REMY JE TE DETESTE'});
 });
 
 module.exports = router;
